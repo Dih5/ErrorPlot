f4645b7753671c53547c401d66d123b486aa7ed3
b3m2a1 <b3m2a1@gmail.com>
2018-12-25 02:48:40 -0500

    I am super dumb

--- ErrorPlot/PacletInfo.m
+++ ErrorPlot/PacletInfo.m
@@ -12,7 +12,7 @@ Paclet[
     Extensions ->
         {
             {"Kernel",
-              "Context"->{"ErrorPlot`"}
+              "Context"->{"ErrorPlot`"},
               "Root"->"."
             },
             {"Documentation", Resources ->
--- PacletInfo.m
+++ PacletInfo.m
@@ -12,7 +12,7 @@ Paclet[
     Extensions ->
         {
           {"Kernel",
-            "Context"->{"ErrorPlot`"}
+            "Context"->{"ErrorPlot`"},
             "Root"->"ErrorPlot"
           },
           {"Documentation", Resources ->
